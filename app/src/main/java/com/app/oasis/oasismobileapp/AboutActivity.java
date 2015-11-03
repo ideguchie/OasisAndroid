b7fb326307c30072e756879bf95b4dca338d3699
lanaphan <lanaphan@uw.edu>
2015-11-02 21:09:57 -0800

    Added About Me section

--- app/src/main/java/com/app/oasis/oasismobileapp/AboutActivity.java
+++ app/src/main/java/com/app/oasis/oasismobileapp/AboutActivity.java
@@ -1,9 +1,12 @@
 package com.app.oasis.oasismobileapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -14,6 +17,25 @@ public class AboutActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public void goToFacebook (View view) {
+        goToUrl("https://www.facebook.com/seattleoasis/");
+    }
+
+    public void goToInstagram (View view) {
+        goToUrl ("https://instagram.com/oasisteazone/");
+    }
+
+    public void goToYelp (View view) {
+        goToUrl("https://www.yelp.com/biz/oasis-tea-zone-seattle/");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
